adb62f59295ec2e22f60c8dc625d4937312e6452
Abel Debela <abel.debela@gmail.com>
2025-12-11 20:18:07 -0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -17,7 +17,7 @@ session = cnx.session()
 fruityvice_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
 # st.text(fruityvice_response).json())
 fv_df = st.dataframe(data=fruityvice_response.json(), use_container_width=True)
-# my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'),col('SEARCH_ON'))
+my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'),col('SEARCH_ON'))
 # st.dataframe(data=my_dataframe, use_container_width=True)
 # st.stop()
 
